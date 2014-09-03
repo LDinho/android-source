e98c31b78fea3d4a5bd068c2ff7f266c51c6cc17
Laurhens D <drhens@gmail.com>
2014-09-03 02:06:30 -0400

    Fix and Change WorstPractices

--- milestone_00_welcome_to_android_development/03_basic_java/best_practices/src/com/bloc/bestpractices/WorstPractices.java
+++ milestone_00_welcome_to_android_development/03_basic_java/best_practices/src/com/bloc/bestpractices/WorstPractices.java
@@ -9,30 +9,39 @@ public class WorstPractices extends Object {
 // EDIT BELOW
 
 	public static void main(String [] args) {
-	int MAGIC_NUMBER = WorstPractices.animals(false);
-	MAGIC_NUMBER *= 5;
-	if (MAGIC_NUMBER > 18) {
-	while(MAGIC_NUMBER > 0)
-	{
-	MAGIC_NUMBER--;
-	}
-	}
+
+		int MAGIC_NUMBER = WorstPractices.animals(false);
+
+		MAGIC_NUMBER *= 5;
+
+		if (MAGIC_NUMBER > 18) {
+
+			while(MAGIC_NUMBER > 0) {
+
+				return MAGIC_NUMBER;
+			}
+		}
 	}
 
-// animals
-// this method takes in a single parameter, yeswellheresthething. Using a very elaborate and complex algorithm, it calculate a magic number
-// yeswellheresthething: a seed which helps generate the magic number
-// returns: a magical number
-private static int animals(boolean yeswellheresthething)
-{
-		/*
-			Start off with one of these
-		*/
-	int anint = yeswellheresthething ? 34 : 21;
-	float sparklesfairy = .5f;
-	for (int brown = 0; brown < aInt; brown++) { sparklesfairy *= aInt;
-	} return (int) sparklesfairy * aInt;
-}
+	/* 	animals
+		this method takes in a single parameter, yesWellHeresTheThing. 
+		Using a very elaborate and complex algorithm, it calculates a magic number
+		yesWellHeresTheThing: a seed which helps generate the magic number
+		returns: a magical number */
 
+	private static int animals(boolean yesWellHeresTheThing) {
+		
+			//Start off with one of these
+
+		boolean yesWellHeresTheThing;
+		int[] aInt = {34, 21};
+		float sparklesFairy = 0.5f;
+
+		for (int i = 0; i < aInt; i++) { 
+
+			sparklesFairy *= aInt;
+
+		} return int sparklesFairy * aInt;
+	}
 // STOP EDITING
 }
