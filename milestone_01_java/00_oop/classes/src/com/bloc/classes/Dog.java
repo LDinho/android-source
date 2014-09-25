b0a297a95774aa2422495f0c1fa78aa8231dbbbc
Laurhens D <drhens@gmail.com>
2014-09-25 00:11:45 -0400

    Add code for Dog Class and Delete Test.java

--- milestone_01_java/00_oop/classes/src/com/bloc/classes/Dog.java
+++ milestone_01_java/00_oop/classes/src/com/bloc/classes/Dog.java
@@ -24,11 +24,20 @@ class Dog {
 
 	// ADD MEMBER VARIABLES HERE IF NECESSARY
 
+	int mMeal = 0;
+	int mPlay = 0;
+
 	/*
 	 * getHairLength
 	 * @return this Dog's hair length
 	 */
 	// ADD YOUR METHOD HERE, NAME MUST MATCH DESCRIPTION
+
+	public float getHairLength() {
+
+		return mHairLength;
+
+	}
 
 	/*
 	 * setHairLength
@@ -38,11 +47,28 @@ class Dog {
 	 */
 	// ADD YOUR METHOD HERE, NAME MUST MATCH DESCRIPTION
 
+	public void setHairLength(float newHairLength) {
+
+		 //logic - cannot be < 0f 
+
+		 if (newHairLength < 0) {
+
+		 	newHairLength = 0f;
+		 }
+
+		 mHairLength = newHairLength;
+	}
+
 	/*
 	 * getGender
 	 * @return this Dog's gender
 	 */
 	// ADD YOUR METHOD HERE, NAME MUST MATCH DESCRIPTION
+
+	public String getGender() {
+
+		return mGender;
+	}
 
 	/*
 	 * setGender
@@ -52,11 +78,22 @@ class Dog {
 	 */
 	// ADD YOUR METHOD HERE, NAME MUST MATCH DESCRIPTION
 
+	public void setGender(String newGender) {
+
+		mGender = newGender;
+
+	}
+
 	/*
 	 * getSize
 	 * @return the size of the dog
 	 */
 	// ADD YOUR METHOD HERE, NAME MUST MATCH DESCRIPTION
+
+	public String getSize() {
+
+		return mSize;
+	}
 
 	/*
 	 * setSize
@@ -66,11 +103,21 @@ class Dog {
 	 */
 	// ADD YOUR METHOD HERE, NAME MUST MATCH DESCRIPTION
 
+	public void setSize(String newSize) {
+
+		mSize = newSize;
+	}
+
 	/*
 	 * getAge
 	 * @return this Dog's age
 	 */
 	// ADD YOUR METHOD HERE, NAME MUST MATCH DESCRIPTION
+
+	public int getAge() {
+
+		return mAge;
+	}
 
 	/*
 	 * setAge
@@ -80,11 +127,21 @@ class Dog {
 	 */
 	// ADD YOUR METHOD HERE, NAME MUST MATCH DESCRIPTION
 
+	public void setAge(int newAge) {
+
+		mAge = newAge;
+	}
+
 	/*
 	 * getWeight
 	 * @return this Dog's weight
 	 */
 	// ADD YOUR METHOD HERE, NAME MUST MATCH DESCRIPTION
+
+	public float getWeight() {
+
+		return mWeight;
+	}
 
 	/*
 	 * setWeight
@@ -94,11 +151,26 @@ class Dog {
 	 */
 	// ADD YOUR METHOD HERE, NAME MUST MATCH DESCRIPTION
 
+	public void setWeight(float newWeight) {
+
+		if (newWeight < MIN_WEIGHT) {
+
+			newWeight = MIN_WEIGHT;	// cannot be < 1.25f 
+		} 
+
+		mWeight = newWeight;  
+	}
+
 	/*
 	 * getColor
 	 * @return this Dog's color
 	 */
 	// ADD YOUR METHOD HERE, NAME MUST MATCH DESCRIPTION
+
+	public String getColor() {
+
+		return mColor;
+	}
 
 	/*
 	 * setColor
@@ -107,6 +179,11 @@ class Dog {
 	 * @return nothing
 	 */
 	// ADD YOUR METHOD HERE, NAME MUST MATCH DESCRIPTION
+
+	public void setColor(String newColor) {
+
+		mColor = newColor;
+	}
 
 	/*
 	 * feed
@@ -118,6 +195,30 @@ class Dog {
 	 */
 	// ADD YOUR METHOD HERE, NAME MUST MATCH DESCRIPTION
 
+	public void feed() {
+
+		mWeight += WEIGHT_GAIN;
+		mMeal++;
+
+		if (mMeal % 3 == 0) {
+
+			if (mSize.equals("tiny")) {
+
+				mSize = "small";
+
+			} else if (mSize.equals("small")) {
+
+				mSize = "average";
+
+			} else if (mSize.equals("average")) {
+
+				mSize = "large";
+			}
+
+		}
+		
+	}
+
 	/*
 	 * play
 	 * Side-effect: 1. The Dog loses weight, specifically WEIGHT_LOSS
@@ -128,6 +229,35 @@ class Dog {
 	 */
 	// ADD YOUR METHOD HERE, NAME MUST MATCH DESCRIPTION
 
+	public void play() {
+
+		// setWeight(mWeight - WEIGHT_LOSS); -- Alternative? Then don't have to add that last 'if' statement as in line 256
+
+		mWeight -= WEIGHT_LOSS;
+		mPlay++;
+
+		if (mPlay % 6 == 0) {
+
+			if (mSize.equals("large")) {
+
+				mSize = "average";
+
+			} else if (mSize.equals("average")) {
+
+				mSize = "small";
+
+			} else if (mSize.equals("small")) {
+
+				mSize = "tiny";
+
+			}
+		}
+
+		if (mWeight < MIN_WEIGHT) {
+			mWeight = MIN_WEIGHT;
+		}	
+	}
+
 	/*
 	 * cutHair
 	 * Side-effect: the Dog's hair length is reduced by HAIR_CUT_LENGTH
@@ -136,4 +266,21 @@ class Dog {
 	 */
 	// ADD YOUR METHOD HERE, NAME MUST MATCH DESCRIPTION
 
+	public void cutHair() {
+
+		mHairLength -= HAIR_CUT_LENGTH;
+
+		if (mHairLength < 0) {
+			mHairLength = 0;
+		}
+
+		// OR ??
+
+		// setHairLength(mHairLength - HAIR_CUT_LENGTH);  Is this correct??
+
+		//
+	}
 }
+
+
+
