60dd426db63e8b074273cb901702c03be401d866
Laurhens D <drhens@gmail.com>
2014-10-16 04:16:07 -0400

    Add code to generics Main.java

--- milestone_01_java/01_polymorphism/generics/src/com/bloc/generics/Main.java
+++ milestone_01_java/01_polymorphism/generics/src/com/bloc/generics/Main.java
@@ -10,6 +10,26 @@ public class Main extends Object {
 		/*
 		 * Put a bunch of Toys in toyBox!
 		 */
+		// Example from generics lesson:
+		// FancyArray<Movie> movieArray = new FancyArray<Movie>();
+		// movieArray.add(new HorrorMovie("Troll 2"));
+		// movieArray.add(new SciFiMovie("Plan 9 From Outer Space")); // why is this correct in lesson??
+		// movieArray.add(new RomanticMovie("You've Got Mail"));
+
+		Toy<ActionFigure> superHeroe = new Toy<ActionFigure>(new ActionFigure());
+		Toy<Book> fantasyBook = new Toy<Book>(new Book());
+		Toy<Spoon> redSpoon = new Toy<Spoon>(new Spoon());
+		Toy<Thing> someThing = new Toy<Thing>(new Thing("SoccerBall"));
+		Toy<ActionFigure> spiderman = new Toy<ActionFigure>(new ActionFigure());
+
+		// toyBox.addToy(new ActionFigure()); // why is this wrong?
+
+		toyBox.addToy(superHeroe);
+		toyBox.addToy(fantasyBook);
+		toyBox.addToy(redSpoon);
+		toyBox.addToy(someThing);
+		toyBox.addToy(spiderman);
+
 
 		assert toyBox.getToyCount() > 0 : "Let's get some toys in that box!";
 		System.out.println("Inside your toybox you've got:");
