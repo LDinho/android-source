c2804beb9b17d5f4969ced3b0293c23ff4379e4d
Laurhens D <drhens@gmail.com>
2014-10-22 11:19:41 -0400

    Add code in FavoritePastries

--- milestone_01_java/01_polymorphism/collections/src/com/bloc/collections/FavoritePastries.java
+++ milestone_01_java/01_polymorphism/collections/src/com/bloc/collections/FavoritePastries.java
@@ -24,8 +24,19 @@ public class FavoritePastries {
 	 * between rating and pastry
 	 */
 
+	// **** Please check my comments too ****
+
+	// Declare instance variable 'favPastry' of type HashMap ??
+	// With Key type "Pastry", and value type "Integer" ??
+
+	private HashMap<Pastry, Integer> favPastry;
+
 	public FavoritePastries() {
 		// WORK HERE
+
+		//Here in this constructor, I create a new HashMap to 'initialize' our new variable 'favPastry'
+
+		favPastry = new HashMap<Pastry, Integer>(); // New pastry object
 	}
 
 	/* 
@@ -43,6 +54,9 @@ public class FavoritePastries {
 	 */
 	public void addPastry(Pastry pastry, int rating) {
 		// WORK HERE
+
+		// From HashMap class, call the put() method to add and associate rating to pastry in FavoritePastries
+		favPastry.put(pastry, rating);
 	}
 
 	/*
@@ -57,7 +71,15 @@ public class FavoritePastries {
 	 */
 	public boolean removePastry(Pastry pastry) {
 		// WORK HERE
-		return false;
+
+		// call containsKey() method to check if our key of type Pastry exist/found from our new HashMap collection
+		// call remove() method to remove pastry
+		if(favPastry.containsKey(pastry)) {
+			favPastry.remove(pastry);
+			return true;
+		} else {
+			return false;
+		}
 	}
 
 	/*
@@ -74,7 +96,13 @@ public class FavoritePastries {
 	 */
 	public int getRatingForPastry(Pastry pastry) {
 		// WORK HERE
-		return -1;
+
+		// call get() to get the rating ?? -- Returns the value to which the specified key is mapped
+		if(favPastry.containsKey(pastry)) {
+			return favPastry.get(pastry);
+		} else {
+			return -1;
+		}
 	}
 
 	/*
@@ -93,7 +121,63 @@ public class FavoritePastries {
 	 */
 	public Collection<Pastry> getPastriesForRating(int rating) {
 		// WORK HERE
-		return null;
-	}
 
+		/*
+		public void markAllTasks(boolean completed) {
+	    for(Task task : mTodaysTasks) {
+	        task.markCompleted(completed);
+	    }
+		}
+		for(declaration : expression) {
+			//Statements
+		}
+		The declaration = type of variable accessed in the arrays. 
+		which is used throughout the “for” block of code, 
+		and will be replaced each time with the array value that is being processed. 
+		The expression = evaluation of the array that we need to loop through. 
+		This expression is either an array variable or a method to return an array.
+
+		for(Pastry pastry : rating) {// statement};
+
+		* keySet() method is used to get a Set view of the keys contained in the map
+
+		* Need to store pastries in an array/Collection/Set?
+
+		* A Set interface is a Collection that cannot contain duplicate elements
+
+		* Java platform contains 3 general-purpose Set implementations: HashSet, TreeSet, and LinkedHashSet.
+
+		* HashSet stores its elements in a random ordered list -- is the best-performing, most popular implementation.
+
+		* (from Oracle) Preferred style is to choose an implementation when a Collection is created 
+			and to immediately assign the new collection to a variable of the corresponding interface type 
+			(or to pass the collection to a method expecting an argument of the interface type).
+
+		* Set<String> s = new HashSet<String>();
+
+		*/
+		
+		Set<Pastry> allRatedPastry = new HashSet<Pastry>();
+
+		for(Pastry pastry : favPastry.keySet()) {	// for each pastry, if we have a rating then add that pastry
+													// to the new HashSet list/collection.
+			if(favPastry.get(pastry) == rating){
+				allRatedPastry.add(pastry);
+			}
+		}
+
+		// Why is the code below wrong??
+
+		// HashSet<Pastry> allPastryRated = new HashSet<Pastry>();
+		// Iterator<Pastry> iterator = allPastryRated.iterator();
+  		//    while(iterator.hasNext()){
+  		//    	if(favPastry.get(iterator) == rating){
+		// 		allPastryRated.add(iterator);
+  //    		}
+  //    	}
+		return allRatedPastry;
+
+		// return null;
+	}
 }
+
