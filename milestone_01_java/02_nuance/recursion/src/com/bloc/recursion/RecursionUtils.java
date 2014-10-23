97a6503d39d9d9dca9f978fd890e8c057b86210e
Laurhens D <drhens@gmail.com>
2014-10-23 00:29:23 -0400

    Add code for Recursion

--- milestone_01_java/02_nuance/recursion/src/com/bloc/recursion/RecursionUtils.java
+++ milestone_01_java/02_nuance/recursion/src/com/bloc/recursion/RecursionUtils.java
@@ -15,6 +15,34 @@ public class RecursionUtils extends Object {
 	 */
 	public static final int findMaxRecursively(List<Integer> numbers) {
 		// IMPLEMENT ME
-		return 0;
+
+		/** if number is < then next index, then return next index 
+		* We are dealing with a generic 'List' type consisting of a list of Integer elements
+		* Implement List method : .size() -- returns the number of elements in this list.
+			Use that for base case
+		* get(int index) -- returns the element at the specified position in this list.
+		* subList(int fromIndex, int toIndex) -- Returns a view of the portion of this list 
+			between the specified fromIndex, inclusive, and toIndex, exclusive.
+
+		* could use Math.max(int a, int b) -- returns larger of a and b : This does comparison work for you
+		**/
+
+		if (numbers.size() == 1) {	// base case?
+
+			return numbers.get(0); // returns the one last element at index zero from the numbers List
+		}
+		/* Math.max(int a, int b) gives you the largest number between a and b
+		 * Here 'a' is the element Integer (at position numbers.get(0))
+		 * And 'b' is the recursive method of Integer type which implements in its parameters the
+			subList(int fromIndex, int toIndex) method from List<E>.
+		 * 'numbers' from findMaxRecursively(List<Int> numbers) calls subList(int, int)
+		 * Then from index '1' to total size() of the List, all elements are compared 
+			against the element at index zero (numbers.get(0)) to return largest number.
+		*/
+		return Math.max(numbers.get(0), findMaxRecursively(numbers.subList(1, numbers.size())));
 	}
 }
+
+
+
+
