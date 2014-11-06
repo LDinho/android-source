2221dd54c068058ff27c37975a5946fd9c3cd81b
Laurhens D <drhens@gmail.com>
2014-11-06 03:44:00 -0500

    Add code for Speakerphone and Main

--- milestone_01_java/02_nuance/singletons/src/com/bloc/singletons/Main.java
+++ milestone_01_java/02_nuance/singletons/src/com/bloc/singletons/Main.java
@@ -1,10 +1,28 @@
 package com.bloc.singletons;
 
+import com.bloc.singletons.listeners.*;
+import com.bloc.singletons.talkers.*;
+
 public class Main extends Object {
 
 	public static void main(String [] args) {
 		// Instantiate some talkers and some listeners
+		Talker sinatra = new Singer();
+		Talker parent = new Parent();
+
+		Listener audience = new AudienceMember();
+		Listener dennis = new Child();
+
 		// Register listeners
+		Speakerphone speakerphone = Speakerphone.get();
+
+		speakerphone.addListener(audience);
+		speakerphone.addListener(dennis);
+
 		// Send messages!
+		// from the shoutMessage() method with @param Talker and the Class <?> (a listener object class)
+
+		speakerphone.shoutMessage(sinatra, AudienceMember.class);
+		speakerphone.shoutMessage(parent, Child.class);
 	}
 }
--- milestone_01_java/02_nuance/singletons/src/com/bloc/singletons/Speakerphone.java
+++ milestone_01_java/02_nuance/singletons/src/com/bloc/singletons/Speakerphone.java
@@ -1,20 +1,44 @@
 package com.bloc.singletons;
 
+import java.util.*;
+
 /*
  * This is a singleton class which maintains communication
  * between classes and Listeners
  */
 public class Speakerphone extends Object {
+
+	// The singleton's static instance
+	private static Speakerphone sSpeakerphone; // Singleton's purpose is to control object creation, limiting it to one object only
 	/*
 	 * get
 	 * @return the singleton instance of Speakerphone
 	 */
+
+	public static Speakerphone get() {
+
+		if (sSpeakerphone == null){
+			sSpeakerphone = new Speakerphone();
+		}
+		return sSpeakerphone;
+	}
 
 	/*
 	 * addListener
 	 * Add a listener to Speakerphone's list
 	 * @param listener an instance of the Listener interface
 	 */
+	private HashSet<Listener> listenersList; // declare set/list of listeners
+
+	private Speakerphone() {
+
+		listenersList = new HashSet<Listener>();
+	}
+
+	public void addListener(Listener listener) {
+
+		listenersList.add(listener);
+	}
 
 	/*
 	 * removeListener
@@ -22,11 +46,27 @@ public class Speakerphone extends Object {
 	 * @param listener the Listener to remove
 	 */
 
+	public void removeListener(Listener listener) {
+
+		listenersList.remove(listener);
+	}
+
 	/*
 	 * shoutMessage
 	 * Sends the message to all of the Listeners tracked by Speakerphone
 	 * @param talker a Talker whose message will be sent
 	 */
+
+	public void shoutMessage(Talker talker) {
+
+		// iterator to cycle through the collection/list, obtaining or removing elements.
+		Iterator<Listener> iterateListeners = listenersList.iterator();
+
+		while (iterateListeners.hasNext()) {
+			iterateListeners.next().onMessageReceived(talker.getMessage());
+
+		}
+	}
 
 	/*
 	 * shoutMessage
@@ -38,11 +78,38 @@ public class Speakerphone extends Object {
 	 *
 	 * HINT: see Class.isAssignableFrom()
 	 *		 http://docs.oracle.com/javase/7/docs/api/java/lang/Class.html#isAssignableFrom(java.lang.Class)
+		=============
+	  -java.lang.Class.isAssignableFrom() determines if the class or interface represented by this Class object 
+	  	is either the same as, or is a superclass or superinterface of, the class or interface represented by 
+	  	the specified Class parameter.
+		=============
 	 */
+
+	public void shoutMessage(Talker talker, Class<?> cls) {
+
+		Iterator<Listener> iterateListeners = listenersList.iterator();
+
+		while (iterateListeners.hasNext()) {
+
+			// getClass() returns the runtime class of this Object -- 
+			// The returned Class object is the object that is locked by static synchronized methods of the represented class.
+			Listener listen = iterateListeners.next();
+			if (cls.isAssignableFrom(listen.getClass())) {
+
+				listen.onMessageReceived(talker.getMessage());
+
+			}
+		}
+	}
 
 	/*
 	 * removeAll
 	 * Removes all Listeners from Speakerphone
 	 */
 
+	public void removeAll() {
+
+		listenersList.clear();
+	}
 }
+
